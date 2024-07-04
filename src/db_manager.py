4ed77677dd37673396a7bd1c69f11302229cd7a5
Maksim Netesov <MrEasters@mail.ru>
2024-07-05 00:48:16 +0300

    add db_manager.py

---
+++ src/db_manager.py
@@ -0,0 +1,19 @@
+import psycopg2
+from src.participant import ChatParticipant
+
+class DBManager:
+    def __init__(self, dbname, user, password, host='localhost', port=5432):
+        self.conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        self.cur = self.conn.cursor()
+
+    def add_participant(self, participant: ChatParticipant):
+        insert_query = """
+            INSERT INTO participants (id, username, avatar_url)
+            VALUES (%s, %s, %s)
+        """
+        self.cur.execute(insert_query, (participant.id, participant.username, participant.avatar_url))
+        self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
